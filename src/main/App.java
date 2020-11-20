0a773c8438cd8ae3385fc72777e0a47f1573089e
Jeff Rescignano <jeff@jeffresc.dev>
2020-11-20 16:52:18 -0500

    Update App.java

--- src/main/App.java
+++ src/main/App.java
@@ -8,20 +8,91 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    private final int SCENE_WIDTH = 500;
-    private final int SCENE_HEIGHT = 250;
+    private final int SCENE_WIDTH = 325;
+    private final int SCENE_HEIGHT = 450;
     private Calculator calculator = new Calculator();
     private TextField entryTextField;
     private TextField sliderTextField;
 
     private Boolean shouldClear = false;
+
+    private void setOperButtonAction(Button b) {
+        b.setOnAction( new EventHandler<ActionEvent>() {
+            public void handle( ActionEvent ae ) {
+                final float f = Float.parseFloat(entryTextField.getText());
+                final String operator = b.getText();
+                switch (operator) {
+                    case Calculator.ADD_OP:
+                    case Calculator.SUB_OP:
+                    case Calculator.MULT_OP:
+                    case Calculator.DIV_OP:
+                    case Calculator.POW_OP:
+                        calculator.setCurrentOperator(operator);
+                        calculator.setCurrentValue(f);
+                        entryTextField.setText(entryTextField.getText()+operator);
+                        shouldClear = true;
+                        break;
+                    case Calculator.SQRT_OP:
+                        calculator.sqrtValue(f);
+                        updateDisplayedValue();
+                        break;
+                    case Calculator.NEG_OP:
+                        calculator.negValue(f);
+                        updateDisplayedValue();
+                        break;
+                    case Calculator.SQ_OP:
+                        calculator.sqValue(f);
+                        updateDisplayedValue();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void useEquals() {
+        final float f = Float.parseFloat(entryTextField.getText());
+        calculator.equals(f);
+        updateDisplayedValue();
+    }
+
+    private void setNumButtonAction(Button b) {
+        b.setOnAction( new EventHandler<ActionEvent>() {
+            public void handle( ActionEvent ae ) {
+                useNumButton(b.getText());
+            }
+        });
+    }
+
+    private void useNumButton(String s) {
+        if (shouldClear) {
+            shouldClear = false;
+            entryTextField.setText(s);
+        } else
+            entryTextField.setText(entryTextField.getText() + s);
+    }
+
+    private void setButtonSize(Control c) {
+        setSizeMan(c, 1, 1);
+    }
+
+    private void setSizeMan(Control c, int x, int y) {
+        c.setMinSize(x * 50, y * 50);
+        c.setPrefSize(x * 50, y * 50);
+        c.setMaxSize(x * 50, y * 50);
+    }
+
+    private void setPos(Control c, int x, int y) {
+        c.setTranslateX(x * 50);
+        c.setTranslateY(y * 50);
+    }
 
     private void updateDisplayedValue() {
         String currentValueStr = Float.toString(calculator.getCurrentValue());
@@ -30,121 +101,54 @@ public class App extends Application {
         entryTextField.setText(currentValueStr);
     }
 
-    private void useEquals() {
-        float f = Float.parseFloat(entryTextField.getText());
-        calculator.equals(f);
-        updateDisplayedValue();
-    }
-
-    private void useOperator(String operator) {
-        float f = Float.parseFloat(entryTextField.getText());
-        switch (operator) {
-            case Calculator.ADD_OP:
-            case Calculator.SUB_OP:
-            case Calculator.MULT_OP:
-            case Calculator.DIV_OP:
-            case Calculator.POW_OP:
-                calculator.setCurrentOperator(operator);
-                calculator.setCurrentValue(f);
-                entryTextField.setText(entryTextField.getText()+operator);
-                shouldClear = true;
-                break;
-            case Calculator.SQRT_OP:
-                calculator.sqrtValue(f);
-                updateDisplayedValue();
-                break;
-            case Calculator.NEG_OP:
-                calculator.negValue(f);
-                updateDisplayedValue();
-                break;
-            case Calculator.SQ_OP:
-                calculator.sqValue(f);
-                updateDisplayedValue();
-                break;
-        }
-    }
-
-    private void useButton(String s) {
-        if (shouldClear) {
-            shouldClear = false;
-            entryTextField.setText(s);
-        } else
-            entryTextField.setText(entryTextField.getText() + s);
-    }
-
     @Override
     public void start(Stage stage) {
         // Entry Text Field
         entryTextField = new TextField();
         entryTextField.setTranslateX(0);
         entryTextField.setTranslateY(0);
+        setSizeMan(entryTextField, 6, 1);
 
         // Square Root Button
         Button buttonSqrt = new Button();
         buttonSqrt.setText(Calculator.SQRT_OP);
-        buttonSqrt.setTranslateX(0);
-        buttonSqrt.setTranslateY(25);
-
-        buttonSqrt.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonSqrt.getText());
-            }
-        });
+        setPos(buttonSqrt, 0, 1);
+        setButtonSize(buttonSqrt);
+        setOperButtonAction(buttonSqrt);
 
         // Power Button
         Button buttonPow = new Button();
         buttonPow.setText(Calculator.POW_OP);
-        buttonPow.setTranslateX(25);
-        buttonPow.setTranslateY(25);
-
-        buttonPow.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonPow.getText());
-            }
-        });
+        setPos(buttonPow, 1, 1);
+        setButtonSize(buttonPow);
+        setOperButtonAction(buttonPow);
 
         // Square Button
         Button buttonSq = new Button();
         buttonSq.setText(Calculator.SQ_OP);
-        buttonSq.setTranslateX(50);
-        buttonSq.setTranslateY(25);
-
-        buttonSq.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonSq.getText());
-            }
-        });
+        setPos(buttonSq, 2, 1);
+        setButtonSize(buttonSq);
+        setOperButtonAction(buttonSq);
 
         // Negate Button
         Button buttonNeg = new Button();
         buttonNeg.setText(Calculator.NEG_OP);
-        buttonNeg.setTranslateX(75);
-        buttonNeg.setTranslateY(25);
-
-        buttonNeg.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonNeg.getText());
-            }
-        });
+        setPos(buttonNeg, 0, 5);
+        setButtonSize(buttonNeg);
+        setOperButtonAction(buttonNeg);
 
         // Decimal Button
         Button buttonDec = new Button();
         buttonDec.setText(".");
-        buttonDec.setTranslateX(75);
-        buttonDec.setTranslateY(50);
-
-        buttonDec.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(buttonDec.getText());
-            }
-        });
+        setPos(buttonDec, 2, 5);
+        setButtonSize(buttonDec);
+        setNumButtonAction(buttonDec);
 
         // Equals Button
         Button buttonEq = new Button();
         buttonEq.setText("=");
-        buttonEq.setTranslateX(75);
-        buttonEq.setTranslateY(100);
-
+        setPos(buttonEq, 3, 5);
+        setButtonSize(buttonEq);
         buttonEq.setOnAction( new EventHandler<ActionEvent>() {
             public void handle( ActionEvent ae ) {
                 useEquals();
@@ -153,181 +157,111 @@ public class App extends Application {
 
         // Mult Button
         Button buttonMult = new Button();
-        buttonMult.setText("*");
-        buttonMult.setTranslateX(100);
-        buttonMult.setTranslateY(25);
-
-        buttonMult.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonMult.getText());
-            }
-        });
+        buttonMult.setText(Calculator.MULT_OP);
+        setPos(buttonMult, 3, 2);
+        setButtonSize(buttonMult);
+        setOperButtonAction(buttonMult);
 
         // Div Button
         Button buttonDiv = new Button();
         buttonDiv.setText(Calculator.DIV_OP);
-        buttonDiv.setTranslateX(100);
-        buttonDiv.setTranslateY(50);
-
-        buttonDiv.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonDiv.getText());
-            }
-        });
+        setPos(buttonDiv, 3, 1);
+        setButtonSize(buttonDiv);
+        setOperButtonAction(buttonDiv);
 
         // Add Button
         Button buttonPlus = new Button();
         buttonPlus.setText(Calculator.ADD_OP);
-        buttonPlus.setTranslateX(100);
-        buttonPlus.setTranslateY(75);
-
-        buttonPlus.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonPlus.getText());
-            }
-        });
+        setPos(buttonPlus, 3, 4);
+        setButtonSize(buttonPlus);
+        setOperButtonAction(buttonPlus);
 
         // Subtract Button
         Button buttonMin = new Button();
         buttonMin.setText(Calculator.SUB_OP);
-        buttonMin.setTranslateX(100);
-        buttonMin.setTranslateY(100);
-
-        buttonMin.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useOperator(buttonMin.getText());
-            }
-        });
+        setPos(buttonMin, 3, 3);
+        setButtonSize(buttonMin);
+        setOperButtonAction(buttonMin);
         
         // 7 Button
         Button button7 = new Button();
         button7.setText("7");
-        button7.setTranslateX(0);
-        button7.setTranslateY(50);
-
-        button7.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button7.getText());
-            }
-        });
+        setPos(button7, 0, 2);
+        setButtonSize(button7);
+        setNumButtonAction(button7);
 
         // 4 Button
         Button button4 = new Button();
         button4.setText("4");
-        button4.setTranslateX(0);
-        button4.setTranslateY(75);
-
-        button4.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button4.getText());
-            }
-        });
+        setPos(button4, 0, 3);
+        setButtonSize(button4);
+        setNumButtonAction(button4);
 
         // 1 Button
         Button button1 = new Button();
         button1.setText("1");
-        button1.setTranslateX(0);
-        button1.setTranslateY(100);
-
-        button1.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button1.getText());
-            }
-        });
+        setPos(button1, 0, 4);
+        setButtonSize(button1);
+        setNumButtonAction(button1);
 
         // 8 Button
         Button button8 = new Button();
         button8.setText("8");
-        button8.setTranslateX(25);
-        button8.setTranslateY(50);
-
-        button8.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button8.getText());
-            }
-        });
+        setPos(button8, 1, 2);
+        setButtonSize(button8);
+        setNumButtonAction(button8);
 
         // 5 Button
         Button button5 = new Button();
         button5.setText("5");
-        button5.setTranslateX(25);
-        button5.setTranslateY(75);
-
-        button5.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button5.getText());
-            }
-        });
+        setPos(button5, 1, 3);
+        setButtonSize(button5);
+        setNumButtonAction(button5);
 
         // 2 Button
         Button button2 = new Button();
         button2.setText("2");
-        button2.setTranslateX(25);
-        button2.setTranslateY(100);
-
-        button2.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button2.getText());
-            }
-        });
+        setPos(button2, 1, 4);
+        setButtonSize(button2);
+        setNumButtonAction(button2);
 
         // 9 Button
         Button button9 = new Button();
         button9.setText("9");
-        button9.setTranslateX(50);
-        button9.setTranslateY(50);
-
-        button9.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button9.getText());
-            }
-        });
+        setPos(button9, 2, 2);
+        setButtonSize(button9);
+        setNumButtonAction(button9);
 
         // 6 Button
         Button button6 = new Button();
         button6.setText("6");
-        button6.setTranslateX(50);
-        button6.setTranslateY(75);
-
-        button6.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button6.getText());
-            }
-        });
+        setPos(button6, 2, 3);
+        setButtonSize(button6);
+        setNumButtonAction(button6);
 
         // 3 Button
         Button button3 = new Button();
         button3.setText("3");
-        button3.setTranslateX(50);
-        button3.setTranslateY(100);
-
-        button3.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button3.getText());
-            }
-        });
+        setPos(button3, 2, 4);
+        setButtonSize(button3);
+        setNumButtonAction(button3);
 
         // 0 Button
         Button button0 = new Button();
         button0.setText("0");
-        button0.setTranslateX(75);
-        button0.setTranslateY(75);
-
-        button0.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle( ActionEvent ae ) {
-                useButton(button0.getText());
-            }
-        });
+        setPos(button0, 1, 5);
+        setButtonSize(button0);
+        setNumButtonAction(button0);
 
         // Clear Button
         Button buttonClear = new Button();
         buttonClear.setText("C");
-        buttonClear.setTranslateX(125);
-        buttonClear.setTranslateY(25);
-
+        setPos(buttonClear, 5, 1);
+        setButtonSize(buttonClear);
         buttonClear.setOnAction( new EventHandler<ActionEvent>() {
             public void handle( ActionEvent ae ) {
                 entryTextField.clear();
+                sliderTextField.clear();
                 calculator.clearValue();
             }
         });
@@ -335,44 +269,40 @@ public class App extends Application {
         // Clear Entry Button
         Button buttonClearEntry = new Button();
         buttonClearEntry.setText("CE");
-        buttonClearEntry.setTranslateX(125);
-        buttonClearEntry.setTranslateY(50);
-
+        setPos(buttonClearEntry, 5, 2);
+        setButtonSize(buttonClearEntry);
         buttonClearEntry.setOnAction( new EventHandler<ActionEvent>() {
             public void handle( ActionEvent ae ) {
                 entryTextField.clear();
             }
         });
 
-        // slider Button
+        // Slider Button
         Button buttonSlider = new Button();
         buttonSlider.setText("Use Slider");
-        buttonSlider.setTranslateX(0);
-        buttonSlider.setTranslateY(185);
+        setPos(buttonSlider, 0, 8);
+        setSizeMan(buttonSlider, 4, 1);
 
         buttonSlider.setOnAction( new EventHandler<ActionEvent>() {
             public void handle( ActionEvent ae ) {
-                useButton(sliderTextField.getText());
+                useNumButton(sliderTextField.getText());
                 sliderTextField.clear();
             }
         });
 
-        // slider Text Field
+        // Slider Text Field
         sliderTextField = new TextField();
-        sliderTextField.setTranslateX(0);
-        sliderTextField.setTranslateY(160);
+        setPos(sliderTextField, 0, 7);
+        setSizeMan(sliderTextField, 4, 1);
 
-        //slider
+        // Slider
         Slider slider = new Slider();
-        slider.setMaxWidth( 350 );
-        slider.setTranslateX(0);
-        slider.setTranslateY(130);
+        setSizeMan(slider, 4, 1);
+        setPos(slider, 0, 6);
         slider.setStyle("-fx-font-size: 30; -fx-text-fill: #EEEEEE;");
-
         slider.valueProperty().addListener( new ChangeListener<Number>() {
             @Override
-            public void changed( ObservableValue<? extends Number> observableValue,
-                                 Number oldValue, Number newValue ) {
+            public void changed( ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue ) {
                 sliderTextField.textProperty().setValue( Integer.toString((int)Math.round((Double) newValue)));
             }
         });
@@ -392,49 +322,54 @@ public class App extends Application {
                         break;
                     case NUMPAD1:
                     case DIGIT1:
-                        useButton("1");
+                        useNumButton("1");
                         break;
                     case NUMPAD2:
                     case DIGIT2:
-                        useButton("2");
+                        useNumButton("2");
                         break;
                     case NUMPAD3:
                     case DIGIT3:
-                        useButton("3");
+                        useNumButton("3");
                         break;
                     case NUMPAD4:
                     case DIGIT4:
-                        useButton("4");
+                        useNumButton("4");
                         break;
                     case NUMPAD5:
                     case DIGIT5:
-                        useButton("5");
+                        useNumButton("5");
                         break;
                     case NUMPAD6:
                     case DIGIT6:
-                        useButton("6");
+                        useNumButton("6");
                         break;
                     case NUMPAD7:
                     case DIGIT7:
-                        useButton("7");
+                        useNumButton("7");
                         break;
                     case NUMPAD8:
                     case DIGIT8:
-                        useButton("8");
+                        useNumButton("8");
                         break;
                     case NUMPAD9:
                     case DIGIT9:
-                        useButton("9");
+                        useNumButton("9");
                         break;
                     case NUMPAD0:
                     case DIGIT0:
-                        useButton("0");
+                        useNumButton("0");
                         break;
                     default:
                         break;
                 }
             }
         });
+
+        // Set CSS
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        scene.setFill(Color.rgb(33,33,33));
+        root.getStyleClass().add("root");
 
         // Stage
         stage.setTitle("Calculator");
